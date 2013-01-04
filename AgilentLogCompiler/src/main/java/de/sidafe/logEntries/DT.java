39d1cdd89b678f8ca960a086c09fad1808eef6fa
Felix Deutschmann <sifedeut@stud.hszg.de>
2013-01-04 08:33:47 +0100

    added new LogEntry
    closes issue #1
    
    Change-Id: I242e7ee8c09944c73c661d0b330c3f7c45bfc58e

---
+++ AgilentLogCompiler/src/main/java/de/sidafe/logEntries/DT.java
@@ -0,0 +1,37 @@
+package de.sidafe.logEntries;
+
+import java.util.ArrayList;
+import de.sidafe.util.ILogEntry;
+
+public class DT implements ILogEntry {
+	private ArrayList<String> headings;
+	private ArrayList<String> values;
+	
+	public DT() {
+		this.headings = new ArrayList<String>(5);
+		this.headings.add("DT_test_status");
+		this.headings.add("DT_test_substatus");
+		this.headings.add("DT_failing_vector_number");
+		this.headings.add("DT_pin_count");
+		this.headings.add("DT_test_designator");
+		
+		this.values = new ArrayList<String>(5);
+		for (int i=0; i<5; i++) {
+			this.values.add("");
+		}
+	}
+	
+	public void setLogEntryValues(ArrayList<String> logEntryValues) {
+		for (int i=0; i<this.headings.size(); i++) {
+			this.values.set(i, logEntryValues.get(i));
+		}
+	}
+
+	public ArrayList<String> getHeadings() {
+		return this.headings;
+	}
+
+	public ArrayList<String> getValues() {
+		return this.values;
+	}
+}
